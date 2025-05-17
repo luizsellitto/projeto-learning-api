20cd923406e3ae7ee76cdac94022a82f8eeb1363
Luizsellitto <luizsellitto2609@gmail.com>
2025-05-17 17:09:21 -0300

    Initial commit

---
+++ src/test/java/com/luizselli/projeto_learning_api/ProjetoLearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizselli.projeto_learning_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
